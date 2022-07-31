a9516449331c0b1597da4fe3b53eb59f41b6a2d1
Trots Anatloliy <Trots.anatoly@gmail.com>
2022-07-31 18:36:02 +0300

    Выполнено 2е задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+    .map(item => 
+        `        
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`  
+    )
+    .join("")
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    console.log(event.target.src);
+
+    const lightBoxImg = new SimpleLightbox('.gallery a', {
+        // sourceAttr: `${event.target.src}`,
+
+    })
+
+    lightBoxImg.open();
+}
